3b0ee9481be830285b6284b652c6ac5b416fd77f
vineet-53 <vineetsinghnegi53@gmail.com>
2024-07-31 20:29:56 +0530

    fixed working of memoryretention game

--- client/src/components/core/MemoryRetentionPage/MemoryRententionTest.jsx
+++ client/src/components/core/MemoryRetentionPage/MemoryRententionTest.jsx
@@ -10,14 +10,14 @@ export default function MemoryRetentionTest() {
   const [level, setLevel] = useState(1);
   const [reveal, setReveal] = useState(resetCards(false));
   const [answers, setAnswers] = useState([]);
-  const cardsRef = useRef(resetCards(null));
-  const highlightDivRef = useRef(null);
-  const dehighlightRef = useRef(null);
   const [index, setIndex] = useState(0);
-  const correctIndex = useRef(0);
   const [win, setWin] = useState(false);
   const [lose, setLose] = useState(false);
-
+  const highlightDivRef = useRef(null);
+  const dehighlightRef = useRef(null);
+  const correctIndex = useRef(0);
+  const cardsRef = useRef(resetCards(null));
+  const enableButtonRef = useRef(false);
   //functions
   const generateRandomNumber = () => {
     return Math.floor(Math.random() * 2) + 1;
@@ -30,7 +30,7 @@ export default function MemoryRetentionTest() {
   };
 
   const highlightDiv = () => {
-    console.log("INDEX VALUE:-", index);
+    // console.log("INDEX VALUE:-", index);
     if (index < answers.length) {
       let [row, col] = answers[index];
       if (row && col) {
@@ -57,10 +57,28 @@ export default function MemoryRetentionTest() {
     setIndex(0);
     correctIndex.current = 0;
     setWin(false);
+    clearInterval(highlightDivRef.current);
+    clearInterval(dehighlightRef.current);
+    enableButtonRef.current = false;
     setLose(false);
+  };
+  const resetGame = () => {
+    setIndex(0);
+    setLevel(1);
+    correctIndex.current = 0;
+    setWin(false);
+    clearInterval(highlightDivRef.current);
+    setLose(false);
+    setAnswers(() => []);
+    enableButtonRef.current = false;
   };
 
   const checkAnswerPattern = (rowIndex, colIndex) => {
+    if (!enableButtonRef.current) {
+      console.log("RESET THE STAGE");
+      resetGame();
+      return;
+    }
     if (correctIndex.current < answers.length) {
       const [row, col] = answers[correctIndex.current];
       if (row === rowIndex && col === colIndex) {
@@ -75,23 +93,21 @@ export default function MemoryRetentionTest() {
   };
 
   useEffect(() => {
-    fillAnswers();
-  }, []);
-
-  useEffect(() => {
     if (answers.length) {
       highlightDiv();
-      console.log("answers", answers);
+      if (index == answers.length) {
+        enableButtonRef.current = true;
+      }
     }
   }, [answers, index]);
 
   useEffect(() => {
-    console.log("ON LEVEL CHANGE");
     fillAnswers();
   }, [level]);
 
   return (
     <PageTestContainer>
+      <div className="text-3xl font-bold text-white ">LEVEL: {level}</div>
       {!lose &&
         reveal.map((row, rowIndex) => {
           return (
@@ -106,7 +122,7 @@ export default function MemoryRetentionTest() {
                       (cardsRef.current[rowIndex][colIndex] = elem)
                     }
                     key={colIndex}
-                    className={`w-20 h-20 cursor-pointe rounded-md bg-blue-200  hover:bg-blue-5`}
+                    className={`w-20 h-20 cursor-pointer rounded-md bg-blue-200  hover:bg-blue-5`}
                   ></div>
                 );
               })}
